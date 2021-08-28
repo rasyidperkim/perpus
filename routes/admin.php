3b66916196c65303e3ef6a0bdb148c9eb478d9c9
Abah Faqih <rasyidperkim@gmail.com>
2021-08-29 01:11:47 +0800

    route author

--- routes/admin.php
+++ routes/admin.php
@@ -8,16 +8,16 @@ use App\Http\Controllers\Admin\DataController;
 
 Route::get('/', [HomeController::class, 'index'])->name('dashboard');
 
-// Route::get('/author', [AuthorController::class, 'index'])->name('author.index');
+Route::get('/author', [AuthorController::class, 'index'])->name('author.index');
 
-// Route::get('/author/create', [AuthorController::class, 'create'])->name('author.create');
+Route::get('/author/create', [AuthorController::class, 'create'])->name('author.create');
 
-// Route::post('/author', [AuthorController::class, 'store'])->name('author.store');
+Route::post('/author', [AuthorController::class, 'store'])->name('author.store');
 
-// Route::get('/author/{author}/edit', [AuthorController::class, 'edit'])->name('author.edit');
+Route::get('/author/{author}/edit', [AuthorController::class, 'edit'])->name('author.edit');
 
-// Route::put('/author/{author}', [AuthorController::class, 'update'])->name('author.update');
+Route::put('/author/{author}', [AuthorController::class, 'update'])->name('author.update');
 
-// Route::delete('/author/{author}', [AuthorController::class, 'destroy'])->name('author.destroy');
+Route::delete('/author/{author}', [AuthorController::class, 'destroy'])->name('author.destroy');
 
-// Route::get('/author/data', [DataController::class, 'authors'])->name('author.data');
+Route::get('/author/data', [DataController::class, 'authors'])->name('author.data');
