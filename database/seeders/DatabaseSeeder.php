bed8e914a1aaea73d886a6c5599977cfe083b1b1
Abah Faqih <rasyidperkim@gmail.com>
2021-08-28 20:01:54 +0800

    Make Author data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(RolesTableSeeder::class);
-        $this->call(AdminUserSeeder::class);
+        // $this->call(RolesTableSeeder::class);
+        // $this->call(AdminUserSeeder::class);
+        // \App\Models\Author::factory(10)->create();
     }
 }
