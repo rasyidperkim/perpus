f0aa2615246e55daeb136a389501f679c983829b
Abah Faqih <rasyidperkim@gmail.com>
2021-08-29 01:11:29 +0800

    config Yajra and publish vendor

--- app/Http/Controllers/Admin/DataController.php
+++ app/Http/Controllers/Admin/DataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class DataController extends Controller
 {
@@ -12,7 +13,7 @@ class DataController extends Controller
 
         $authors = Author::orderBy('name', 'ASC');
         
-        return datatables()->of($authors)
+        return DataTables::eloquent($authors)
                 ->addColumn('action', 'admin.author.action')
                 ->addIndexColumn()
                 ->toJson();
