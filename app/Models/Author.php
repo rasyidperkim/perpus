2f7aa6d95a1db42796935ec70839e3da509ea0aa
Abah Faqih <rasyidperkim@gmail.com>
2021-09-04 01:09:20 +0800

    hasMany relationship and cover setting conditional

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -11,4 +11,9 @@ class Author extends Model
 
     protected $guarded = [];
     public $timestamps = false;
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
