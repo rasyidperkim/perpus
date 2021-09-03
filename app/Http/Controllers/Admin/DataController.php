b3e50d35cc58971376fccc3c7bea17892f17d933
Abah Faqih <rasyidperkim@gmail.com>
2021-09-04 01:05:15 +0800

    Add Column cover, author, qty, action in Books

--- app/Http/Controllers/Admin/DataController.php
+++ app/Http/Controllers/Admin/DataController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 
@@ -16,6 +17,25 @@ class DataController extends Controller
         return DataTables::eloquent($authors)
                 ->addColumn('action', 'admin.author.action')
                 ->addIndexColumn()
+                ->rawColumns(['action'])
+                ->toJson();
+    }
+    
+    public function books(){
+
+        
+        $books = Book::orderBy('title', 'ASC');
+        
+        return DataTables::eloquent($books)
+                ->addColumn('author', function(Book $model){
+                    return $model->author->name;
+                })
+                ->editColumn('cover', function(Book $model) {
+                    return '<center><img src="' . $model->getCover() . '" height="150px" width="100%" /></center>';
+                })
+                ->addColumn('action', 'admin.book.action')
+                ->addIndexColumn()
+                ->rawColumns(['cover', 'action'])
                 ->toJson();
     }    
     
